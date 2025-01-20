10d0b9424d62f631afedfbcf1e572b9b0972f5f7
MostafaMnesey <mneseym@gmail.com>
2025-01-20 15:00:14 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,18 @@ import Home from './Comps/Home/Home';
 import About from './Comps/About/About';
 import Contact from './Comps/Contact/Contact';
 import Gallary from './Comps/Gallary/Gallary';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Layout from './assets/Layout/Layout';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
  {path:'',element:<Layout/>,children:[
   {index:true ,element:<Home/>},
   {path:'about' ,element:<About/>},
   {path:'contact' ,element:<Contact/>},
   {path:'gallary' ,element:<Gallary/>},
+  {path:'*' ,element:<>404</>},
+  
+  
  ]
  }
 ])
